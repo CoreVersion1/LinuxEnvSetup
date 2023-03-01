a243c82e422a0c47b9917442aa451f6046fa6029
ZM <systemcore@139.com>
2023-03-01 20:51:09 +0800

    add Ubuntu setup;

---
+++ Ubuntu/linux_env_setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get upgrade -y
+
+# basic tools
+sudo apt-get install -y \
+net-tools \
+unzip \
+vim \
+mlocate \
+tree \
+tmux
+
+# develop depenencies
+sudo apt-get install -y \
+git \
+cmake \
+cmake-curses-gui
